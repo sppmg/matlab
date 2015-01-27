6df03ead4d90563eee5e432bb2d3da30fdc0e57c
sppmg <sm.sppmg@gmail.com>
2015-01-27 21:06:58 +0800

    Backup. Rewrite from worked program.

--- daq/ECGProc.m
+++ daq/ECGProc.m
@@ -1,12 +1,11 @@
 function ECGProc(obj)
-	persistent mon fig peakhigh old_sd_time;
+	persistent mon fig uiPeakHigh analyze ;
 	if nargin == 0
 		% Action by direct run this function file.
-
-        %delete(timerfind); close all; clear classes;
+        % delete(timerfind); close all; clear classes;
 		daq = daqmx_Task('chan','dev1/ai1','rate',5000,'callbackfunc','ECGProc','ProcPeriod',1);
 		daq.DataWindowLen = 120*daq.Rate ;			% store 120 sec. data in object.
-		% daq.SampleNum=round(3*daq.Rate*daq.ProcPeriod);	% 3 times ProcPeriod buffer. Should default setting in last daqmx_Task.
+		% daq.SampleNum=round(3*daq.Rate*daq.ProcPeriod);	% 3 times ProcPeriod buffer. Should be default setting in last daqmx_Task.
 		daq.ResetDev;
 		daq.start;
 		%pause(10);
@@ -14,47 +13,54 @@ function ECGProc(obj)
 		%figure; plot(daq.DataTime , daq.data('a') ); % plot ecg_ra only .
 		%daq.stop;
 	else
-		% Careful , this section variable scope is different with above. <-- because used clear all .
+		% Careful , this section variable scope is different with above. <-- maybe used clear all ?
 		if obj.DataTotalNumPerChan > 0
 			if ~isa(mon,'monitor')
 				% Initialization
-				fig=figure('Renderer','OpenGL') ;
-				peakhigh=uicontrol('style','edit','string','0.2','Parent',fig,'DeleteFcn',{@delme,obj});
-				old_sd_time=0;
+				analyze.rt_msdDataLen = 5 ; % movingstd window. number of data.
+				analyze.rt_peakHigh = 0.2 ; %
+				analyze.rt_peakMinInterval = 0.18 ; % sec.
+				analyze.LastTime = 0 ;
+				analyze.data_length = 60 ; %sec
+				analyze.period = 10 ;
+
+				fig=figure('Renderer','OpenGL','MenuBar', 'none','ToolBar', 'none') ;
+				uiPeakHigh=uicontrol('style','edit','string',analyze.rt_peakHigh,'Parent',fig,'DeleteFcn',{@delme,obj});
 				mon=monitor(fig,obj.DataTime,obj.data);
 			else
 				% Action when every loop , call by timer .
-				if obj.DataTime(end) > 1
-					
-					peak_high=str2num(get(peakhigh,'string')) ;
-					[pks,locs] =findpeaks(sign(peak_high)*obj.data,'MINPEAKHEIGHT',abs(peak_high),'MINPEAKDISTANCE',0.18*obj.Rate);
+				if obj.DataTime(end) > 1	% Analyze after 1 sec.
+					analyze.rt_peakHigh=str2num(get(uiPeakHigh,'string')) ;
+					[pks,locs] =findpeaks(sign(analyze.rt_peakHigh)*obj.data,'MINPEAKHEIGHT',abs(analyze.rt_peakHigh), 'MINPEAKDISTANCE', analyze.rt_peakMinInterval * obj.Rate);
 					ibi=[obj.DataTime(locs(2:end)), diff(obj.DataTime(locs),1)];
-					if length(ibi)<(2*5+1)
-						ibi=[0,0];
+					if length(ibi)<(2* analyze.rt_msdDataLen+1)  % check for movingstd ()
+						%ibi=[0,0];
 						mstd=ibi(:,2);
-						
 					else
-						mstd=movingstd(ibi(:,2),5,'c');
+						mstd=movingstd(ibi(:,2), analyze.rt_msdDataLen,'c');
 					end
-					%mstd=ibi(:,2);
 				else
 					ibi=[0,0];
 					mstd=ibi(:,2);
                 end
 
-                part_start=numel(obj.DataTime)-5*obj.Rate;
+                part_start=numel(obj.DataTime)-5*obj.Rate; % for raw signal in plot
                 if part_start < 1
                     part_start = 1;
                 end
                 d=obj.data;
-                mon.plot(fig,{obj.DataTime(part_start:end),ibi(:,1),ibi(:,1)},{d(part_start:end),ibi(:,2),mstd });
-				if obj.DataTime(end) > old_sd_time+5
-					fprintf('Mean = %f \t HRV = %f \n',mean(ibi(:,2)),std(ibi(:,2))  );
-					%fprintf('HRV = %f \n',));
-					old_sd_time=old_sd_time+5;
+
+                mon.plot(fig, ...
+					{obj.DataTime(part_start:end),ibi(:,1),ibi(:,1)}, ...
+					{d(part_start:end),ibi(:,2),mstd });
+
+				if obj.DataTime(end) > analyze.LastTime + analyze.period
+					analyze.data_ss = ibi(:,1) > obj.DataTime(end) - analyze.data_length ;
+
+					fprintf('IBI = %f \t HRV = %e \n',mean(ibi(analyze.data_ss ,2)),std(ibi(analyze.data_ss ,2))  );
+
+					analyze.LastTime = analyze.LastTime + analyze.period
 				end
-					
-				%mon.plot(fig,{obj.DataTime,ibi(:,1),ibi(:,1)},{obj.data,ibi(:,2),mstd });
 			end
 		end
 	end
