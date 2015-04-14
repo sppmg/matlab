6bb9996c69b0e0a3f875db26f322dcac224a7349
sppmg <sm.sppmg@gmail.com>
2015-04-14 14:11:01 +0800

    Last single channel version.

--- daq/ECGProc.m
+++ daq/ECGProc.m
@@ -1,4 +1,5 @@
 function ECGProc(daq)
+	% need monitor.m before 2015/04/10
 	persistent mon ana ui ;
 	% ui for GUI headle , ana for all analysis variable, .rt_ for real time , .lt_ for long time.
 	if nargin == 0
@@ -9,6 +10,7 @@ function ECGProc(daq)
 		% daq.SampleNum=round(3*daq.Rate*daq.ProcPeriod);	% 3 times ProcPeriod buffer. Should be default setting in last daqmx_Task.
 		daq.ResetDev;
 		daq.start;
+		ana.taskTime = tic ;
 		%pause(10); daq.stop;
 	else
 	% Section action by daq object callback
@@ -24,22 +26,22 @@ function ECGProc(daq)
 				ana.lt_a_updateTime = ana.lt_a_updatePeriod ;
 				ana.lt_a_dataLenSec = 60 ; %sec
 
-				ui.plotPlace = figure('Renderer','OpenGL','MenuBar', 'none','ToolBar', 'none') ;
+				ui.plotPlace = figure('Renderer','OpenGL','MenuBar', 'none','ToolBar', 'none''Units','normalized','Position',[0.6,0.035,0.4,0.91]) ;
 				ui.peakHigh_edt = uicontrol('style','edit','string',ana.rt_peakHigh,'Parent',ui.plotPlace,'DeleteFcn',{@delme,daq});
 				mon = monitor(ui.plotPlace,daq.DataTime,daq.data);
 			else
 				% Action when every loop , call by timer .
 				ana.rt_peakHigh = str2num(get(ui.peakHigh_edt,'string')) ;
 				[pks,locs] = findpeaks( sign(ana.rt_peakHigh)*daq.data, 'MINPEAKHEIGHT', abs(ana.rt_peakHigh), 'MINPEAKDISTANCE', ana.rt_peakMinInterval * daq.Rate);
-				if length(locs) > 2
+				if numel(locs) > 2 % need rewrite this line in 2+ channel.
 					ibi=[daq.DataTime(locs(2:end)), diff(daq.DataTime(locs),1)];
-					if numel(ibi) > (2* ana.rt_msdDataLen+1)  % check for movingstd ()
+					if length(ibi) > (2* ana.rt_msdDataLen+1)  % check for movingstd () , use length here , not numel.
 						mstd=movingstd(ibi(:,2), ana.rt_msdDataLen,'c');
 					else
 						mstd=ibi(:,2);
 					end
 				else
-					ibi=[0,0];
+					ibi=ones(2);
 					mstd=ibi(:,2);
 				end
 
@@ -49,13 +51,26 @@ function ECGProc(daq)
                 end
                 d=daq.data;
 
+				% check BS
+				ibi_rate_last=ibi(2:end,2)./ibi(1:end-1,2);% daq.DataTime(end-length(ibi_rate_last)+1:end) == ibi(2:end,1)
+				ibi_rate_mean=ibi(2:end,2)/mean(ibi(:,2));
+				if ~isnumeric(ibi_rate_last) ; ibi_rate_last=0; end
+				if ~isnumeric(ibi_rate_mean) ; ibi_rate_mean=0; end
+%,daq.DataTime(end-length(ibi_rate_mean)+1:end)
+% ...
+% daq.DataTime(end-length(ibi_rate_last)+1:end)
+% ibi(:,1)
+% ibi_rate_last
+% ibi_rate_mean
+
                 mon.plot(ui.plotPlace, ...
-					{daq.DataTime(part_start:end),ibi(:,1),ibi(:,1)}, ...
-					{d(part_start:end),ibi(:,2),mstd });
+					{daq.DataTime(part_start:end),ibi(:,1),ibi(:,1),daq.DataTime(end-length(ibi_rate_last)+1:end),daq.DataTime(end-length(ibi_rate_mean)+1:end)}, ...
+					{d(part_start:end),ibi(:,2),mstd ,ibi_rate_last,ibi_rate_mean});
 
 				if daq.DataTime(end) > ana.lt_a_updateTime
 					ana.dataSS = ibi(:,1) > ( daq.DataTime(end) - ana.lt_a_dataLenSec );
-					fprintf('IBI = %f \t HRV = %e \n',mean(ibi(ana.dataSS ,2)),std(ibi(ana.dataSS ,2))  );
+					fprintf('Time = %d \t IBI = %f \t HRV = %e \n',toc(ana.taskTime) ,mean(ibi(ana.dataSS ,2)),std(ibi(ana.dataSS ,2))  );
+					% time func--> datestr(now)
 					ana.lt_a_updateTime = ana.lt_a_updateTime + ana.lt_a_updatePeriod
 				end
 			end
